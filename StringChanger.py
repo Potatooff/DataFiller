8de77769000169a2648f017ded0f36c60d53384a
Cloud <112786274+Potatooff@users.noreply.github.com>
2023-08-26 18:15:50 -0400

    Update StringChanger.py

--- StringChanger.py
+++ StringChanger.py
@@ -11,7 +11,7 @@ model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_dev
 # num beans is like the diversity of the meaning The higher it is the more lost it can get the less it is the more accurate it can be
 def get_response(input_text,num_return_sequences,num_beams):  
   batch = tokenizer([input_text],truncation=True,padding='longest',max_length=200, return_tensors="pt").to(torch_device)
-  translated = model.generate(**batch,max_length=150,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)  # temperature is if you want the bot to be more create set it more than 5 else dont change
+  translated = model.generate(**batch,max_length=150,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1)  # temperature is if you want the bot to be more create set it more than 5 else dont change
   tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
   return tgt_text
 
